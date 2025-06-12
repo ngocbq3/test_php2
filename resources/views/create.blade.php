351a1376d8ba914b667306fa8803fcfa65045463
ngocbq3 <ngocbq@fpt.edu.vn>
2025-06-12 21:39:25 +0700

    form create

---
+++ resources/views/create.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('title')
+    Thêm sản phẩm
+@endsection
+
+@section('content')
+    <div class="container w-60">
+        <h1>Thêm mới sản phẩm</h1>
+        <form action="" method="post" enctype="multipart/form-data">
+            <div class="mb-3">
+                <label for="" class="form-label">Name</label>
+                <input type="text" name="name" id="" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Category</label>
+                <select name="category_id" id="" class="form-control">
+                    @foreach ($categories as $cate)
+                        <option value="{{ $cate->id }}">
+                            {{ $cate->name }}
+                        </option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Img thumbnail</label>
+                <input type="file" name="img_thumbnail" id="" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Description</label>
+                <textarea name="description" rows="10" class="form-control"></textarea>
+            </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Create</button>
+            </div>
+        </form>
+    </div>
+@endsection
